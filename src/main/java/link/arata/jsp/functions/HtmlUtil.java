84e382f7f9749089375594d9f0b0e8cda89c69fe
webarata3 <webarata3@gmail.com>
2015-12-17 04:46:07 +0900

    newLineToBrメソッドの追加

--- src/main/java/link/arata/jsp/functions/HtmlUtil.java
+++ src/main/java/link/arata/jsp/functions/HtmlUtil.java
@@ -1,5 +1,11 @@
 package link.arata.jsp.functions;
 
+/**
+ * HTML操作関連のユーティリティークラス
+ * 
+ * @author arata
+ *
+ */
 public abstract class HtmlUtil {
     /**
      * 文字列をHTMLエスケープする
@@ -13,6 +19,21 @@ public abstract class HtmlUtil {
         value = value.replace("<", "&lt;");
         value = value.replace(">", "&gt;");
         value = value.replace("\"", "&quot;");
+
+        return value;
+    }
+
+    /**
+     * 改行コードを&lt;br&gt;に変換する。
+     * 
+     * @param value
+     *            変換する文字列
+     * @return 返還後の文字列
+     */
+    public static String newLineToBr(String value) {
+        value = value.replace("\r\n", "<br>");
+        value = value.replace("\n", "<br>");
+        value = value.replace("\r", "<br>");
 
         return value;
     }
--- src/test/java/link/arata/jsp/functions/HtmlUtilTest.java
+++ src/test/java/link/arata/jsp/functions/HtmlUtilTest.java
@@ -30,4 +30,19 @@ public class HtmlUtilTest {
     public void escapeメソッドでアンドがエスケープされることのテスト() {
         assertThat(HtmlUtil.escape("&"), is("&amp;"));
     }
+
+    @Test
+    public void escapeメソッドで複数の文字列が正しくエスケープされることのテスト() {
+        assertThat(HtmlUtil.escape("<>\"&"), is("&lt;&gt;&quot;&amp;"));
+    }
+
+    @Test
+    public void escapeメソッドで通常の文字列も含んだテスト() {
+        assertThat(HtmlUtil.escape("<div>\"drag&drop\"</div>"), is("&lt;div&gt;&quot;drag&amp;drop&quot;&lt;/div&gt;"));
+    }
+
+    @Test
+    public void newLineToBrメソッドで￥r￥nが正しく変換されること() {
+        assertThat(HtmlUtil.newLineToBr("\r\n"), is("<br>"));
+    }
 }
