9502b94a23e94d07b8d6993405094fdde35acec3
webarata3 <webarata3@gmail.com>
2015-12-18 15:10:40 +0900

    urlToHrefの追加

--- src/main/java/link/arata/jsp/functions/HtmlUtil.java
+++ src/main/java/link/arata/jsp/functions/HtmlUtil.java
@@ -7,45 +7,56 @@ package link.arata.jsp.functions;
  *
  */
 public abstract class HtmlUtil {
-    /**
-     * 文字列をHTMLエスケープする
-     * 
-     * @param value
-     *            エスケープする文字列
-     * @return HTMLエスケープしたあとの文字列
-     */
-    public static String escape(String value) {
-        value = value.replace("&", "&amp;");
-        value = value.replace("<", "&lt;");
-        value = value.replace(">", "&gt;");
-        value = value.replace("\"", "&quot;");
+	/**
+	 * 文字列をHTMLエスケープする
+	 * 
+	 * @param value
+	 *            エスケープする文字列
+	 * @return HTMLエスケープしたあとの文字列
+	 */
+	public static String escape(String value) {
+		value = value.replace("&", "&amp;");
+		value = value.replace("<", "&lt;");
+		value = value.replace(">", "&gt;");
+		value = value.replace("\"", "&quot;");
 
-        return value;
-    }
+		return value;
+	}
 
-    /**
-     * 改行コードを&lt;br&gt;に変換する。
-     * 
-     * @param value
-     *            変換する文字列
-     * @return 変換後の文字列
-     */
-    public static String newLineToBr(String value) {
-        value = value.replace("\r\n", "<br>");
-        value = value.replace("\n", "<br>");
-        value = value.replace("\r", "<br>");
+	/**
+	 * 改行コードを&lt;br&gt;に変換する。
+	 * 
+	 * @param value
+	 *            変換する文字列
+	 * @return 変換後の文字列
+	 */
+	public static String newLineToBr(String value) {
+		value = value.replace("\r\n", "<br>");
+		value = value.replace("\n", "<br>");
+		value = value.replace("\r", "<br>");
 
-        return value;
-    }
+		return value;
+	}
 
-    /**
-     * 半角スペースを&amp;nbsp;に変化sんする。
-     * 
-     * @param value
-     *            変換する文字列
-     * @return 変換後の文字列
-     */
-    public static String blankToNbsp(String value) {
-        return value.replace(" ", "&nbsp;");
-    }
+	/**
+	 * 半角スペースを&amp;nbsp;に変換する。
+	 * 
+	 * @param value
+	 *            変換する文字列
+	 * @return 変換後の文字列
+	 */
+	public static String blankToNbsp(String value) {
+		return value.replace(" ", "&nbsp;");
+	}
+
+	/**
+	 * 文字列中のURLをHTMLのアンカーに変換する
+	 * 
+	 * @param value
+	 *            変換する文字列
+	 * @return 変換後の文字列
+	 */
+	public static String urlToHref(String value) {
+		return value.replaceAll("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?", "<a href=\"$0\">$0</a>");
+	}
 }
